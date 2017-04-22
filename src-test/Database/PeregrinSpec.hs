98b3996bbd227fd90f0b374474d6770f067009e8
Bardur Arantsson <bardur@scientician.net>
2017-04-22 11:29:40 +0200

    Add support for parameterized migrations

--- src-test/Database/PeregrinSpec.hs
+++ src-test/Database/PeregrinSpec.hs
@@ -13,7 +13,8 @@ import           Database.Peregrin
 import           Database.Peregrin.Metadata
 import           Data.Pool (Pool, withResource, destroyAllResources)
 import           Data.Text (Text)
-import           Database.PostgreSQL.Simple (Connection, Query, Only(..), FromRow)
+import           Database.PostgreSQL.Simple (Connection, Query, Only(..), FromRow, ToRow)
+import           Database.PostgreSQL.Simple.Types (Identifier(..))
 import qualified Database.PostgreSQL.Simple as PS
 import           Test.Hspec (Spec, Selector, describe)
 import qualified Test.Hspec as Hspec
@@ -30,39 +31,70 @@ mkMigrateSpec mkConnectionPool =
     describe ("migrate" ++ extra) $ do
       it "can apply a single migration" $ do
         -- Exercise:
-        migrate' schema [ (cid0, createXSql) ]
+        migrate' schema [ (cid0, createXSql, ()) ]
         -- Verify:
         assertCanSelectFromX
 
       it "ignores migrations that have already been applied (single call)" $ do
         -- Exercise:
-        migrate' schema [ (cid0, createXSql)
-                        , (cid0, createXSql) -- Would fail if applied again
+        migrate' schema [ (cid0, createXSql, ())
+                        , (cid0, createXSql, ()) -- Would fail if applied again
                         ]
         -- Verify:
         assertCanSelectFromX
 
       it "ignores migrations that have already been applied (multiple calls)" $ do
         -- Exercise:
-        migrate' schema [ (cid0, createXSql) ]
-        migrate' schema [ (cid0, createXSql) ] -- Would fail if applied again
+        migrate' schema [ (cid0, createXSql, ()) ]
+        migrate' schema [ (cid0, createXSql, ()) ] -- Would fail if applied again
         -- Verify:
         assertCanSelectFromX
 
       it "throws an error if SQL is changed for a given change set ID" $ do
         -- Exercise:
-        migrate' schema [ (cid0, createXSql) ]
-        migrate' schema [ (cid0, createXSqlBad) ]
+        migrate' schema [ (cid0, createXSql, ()) ]
+        migrate' schema [ (cid0, createXSqlBad, ()) ]
           -- Verify: Should throw here
           `shouldThrow` (== MigrationModifiedError cid0)
 
       it "can apply multiple distinct migrations in a single call" $ do
         -- Exercise
-        migrate' schema [ (cid0, createXSql)
-                        , (cid1, createYSql)
+        migrate' schema [ (cid0, createXSql, ())
+                        , (cid1, createYSql, ())
                         ]
         -- Verify: Make sure both migrations have been applied
         assertCanSelectFromXY
+
+      it "can apply parameterized migrations" $ do
+        -- Setup
+        let table = Only $ Table schema "X"
+        -- Exercise
+        migrate' schema [ (cid0, createTableSql, table)
+                        ]
+        -- Verify:
+        assertCanSelectFromP table
+
+      it "can apply identical parameterized migrations with different parameters" $ do
+        -- Setup:
+        let tableX = Table schema "X"
+        let tableY = Table schema "Y"
+        -- Exercise:
+        migrate' schema [ (cid0, createTableSql, Only tableX)
+                        , (cid1, createTableSql, Only tableY)
+                        ]
+        -- Verify:
+        assertCanSelectFromPP (tableX, tableY)
+
+      it "can apply parameterized migrations with different parameter 'shapes'" $ do
+        -- Setup:
+        let tableX = Table schema "X"
+        let tableY = Table schema "Y"
+        -- Exercise:
+        migrate' schema [ (cid0, "CREATE TABLE ? (X INT)", QP $ Only $ tableX)
+                        , (cid1, "CREATE TABLE ? (? INT)", QP $ (tableY, Identifier "Y"))
+                        ]
+        -- Verify:
+        assertCanSelectFromPP (tableX, tableY)
 
   where
     it msg action = Hspec.it msg $
@@ -72,32 +104,42 @@ mkMigrateSpec mkConnectionPool =
     createYSql = "CREATE TABLE Y (B INT)"
     createXSqlBad = "CREATE TABLE X (Y CHAR(1))"
 
+    createTableSql = "CREATE TABLE ? (X INT)"
+
     assertCanSelectFromX =
       assertCanQuery_ "SELECT * FROM X"
+    assertCanSelectFromP p =
+      assertCanQuery "SELECT * FROM ?" p
+    assertCanSelectFromPP p =
+      assertCanQuery "SELECT * FROM ?, ?" p
 
     assertCanSelectFromXY =
-      assertCanQuery_ "SELECT * FROM X, Y where X.A = Y.B"
+      assertCanQuery_ "SELECT * FROM X, Y"
 
     cid0 = "a328156d-9875-4471-8192-0c86959badb3"
     cid1 = "00c6159c-c7f6-4cec-b63f-f70c1c4c7bb1"
 
 assertCanQuery_ :: Query -> ReaderT (Pool Connection) IO ()
 assertCanQuery_ q = do
-  _ :: [Only Int] <- query_ q
+  assertCanQuery q ()
+
+assertCanQuery :: ToRow p => Query -> p -> ReaderT (Pool Connection) IO ()
+assertCanQuery q p = do
+  _ :: [Only Int] <- query q p
   return ()
 
-query_ :: FromRow a => Query -> ReaderT (Pool Connection) IO [a]
-query_ sql = do
+query :: (ToRow p, FromRow a) => Query -> p -> ReaderT (Pool Connection) IO [a]
+query q p = do
   connectionPool <- ask
   withResource connectionPool $ \connection ->
-    lift $ PS.query_ connection sql
+    lift $ PS.query connection q p
 
 shouldThrow :: Exception e => ReaderT (Pool Connection) IO a -> Selector e -> ReaderT (Pool Connection) IO ()
 shouldThrow action selector = do
   connectionPool <- ask
   lift (Hspec.shouldThrow (runReaderT action connectionPool) selector)
 
-migrate' :: Schema -> [(Text, Text)] -> ReaderT (Pool Connection) IO ()
+migrate' :: ToRow p => Schema -> [(Text, Query, p)] -> ReaderT (Pool Connection) IO ()
 migrate' schema migrations = do
   connectionPool <- ask
   lift $ withResource connectionPool $ \connection ->
--- src/Database/Peregrin.hs
+++ src/Database/Peregrin.hs
@@ -1,27 +1,29 @@
+{-# LANGUAGE ExistentialQuantification #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Database.Peregrin
     ( migrate
     , MigrationError(..)
+    , QP(..)
     ) where
 
 import           Control.Applicative ((<$>))
 import           Control.Exception (Exception, throwIO)
 import           Control.Monad (forM_, when, void)
-import           Database.Peregrin.Metadata
+import           Data.ByteString (ByteString)
 import           Data.Text (Text)
-import qualified Data.Text as T
 import           Data.Int (Int32, Int64)
 import           Data.Maybe (listToMaybe, fromMaybe)
-import           Data.String (fromString)
-import           Database.PostgreSQL.Simple (Connection, Only(..), Query)
+import           Database.Peregrin.Metadata
+import           Database.PostgreSQL.Simple (Connection, Only(..), formatQuery)
 import qualified Database.PostgreSQL.Simple as P
 import           Database.PostgreSQL.Simple.ToRow (ToRow(..))
+import           Database.PostgreSQL.Simple.Types (Query(..))
 import           Database.PostgreSQL.Simple.FromRow (FromRow(..), field)
 import           Database.PostgreSQL.Simple.Transaction (withTransactionLevel, IsolationLevel(..))
 
 -- | Migration information stored in 'migration' table.
-data Migration = Migration Text Text
+data Migration = Migration Text ByteString
 
 instance FromRow Migration where
   fromRow = Migration <$> field <*> field
@@ -41,10 +43,17 @@ data MigrationContext = MigrationContext { mcMetaMigrationTable :: Table
                                          , mcMigrationTable :: Table
                                          }
 
+-- | Parameter wrapper for a query. Used in when there are several
+-- sets of parameters which must have the same type.
+data QP = forall p . ToRow p => QP p
+
+instance ToRow QP where
+  toRow (QP qp) = toRow qp
+
 -- | Apply a list of migrations to a database. For example,
 --
--- > migrate conn schema [("a", "CREATE TABLE ...")]
--- >                     [("b", "INSERT INTO TABLE ...")]
+-- > migrate conn schema [("a", "CREATE TABLE ...", QP $ Only $ Table schema "foo")]
+-- >                     [("b", "INSERT INTO TABLE ...", QP $ (Table schema "foo", "bar"))]
 --
 -- will apply the given SQL statements __in order__ and track them by
 -- the identifiers "a" and "b". It is recommended to use __fixed__,
@@ -53,6 +62,10 @@ data MigrationContext = MigrationContext { mcMetaMigrationTable :: Table
 -- unique within the given schema. For example, on a Linux system you
 -- can run the command `uuidgen -r` on the command line and paste that
 -- into your migration list.
+--
+-- If the parameter sets are all of the same "shape" (type), then the
+-- `P $` prefix may be omitted — it serves only to make sure that
+-- the types match up.
 --
 -- The given 'Schema' parameter indicates the schema used for the
 -- /metadata/ stored to track which migrations have been applied. It
@@ -68,7 +81,7 @@ data MigrationContext = MigrationContext { mcMetaMigrationTable :: Table
 -- "@\__peregrin_migration_meta\__@" and "@\__peregrin_migration\__@",
 -- which will automatically be created in the given 'Schema'.
 --
-migrate :: Connection -> Schema -> [(Text, Text)] -> IO ()
+migrate :: ToRow p => Connection -> Schema -> [(Text, Query, p)] -> IO ()
 migrate connection schema =
     migrate' tables connection schema
   where
@@ -76,7 +89,7 @@ migrate connection schema =
                               , mcMigrationTable = Table schema "__peregrin_migration__"
                               }
 
-migrate' :: MigrationContext -> Connection -> Schema -> [(Text, Text)] -> IO ()
+migrate' :: ToRow p => MigrationContext -> Connection -> Schema -> [(Text, Query, p)] -> IO ()
 migrate' tables c schema migrations = do
   -- Must always create the "migration_meta" table (and its
   -- schema) if necessary. Having just created this table without
@@ -97,8 +110,10 @@ migrate' tables c schema migrations = do
   -- Apply all the migrations; we do it one-by-one since our lock is
   -- itself automatically released by PostgreSQL at the end of each of
   -- each transaction.
-  forM_ migrations $ \(mid, sql) ->
+  forM_ migrations $ \(mid, q, p) ->
     withLock $ do
+      -- Subsitute parameters
+      sql <- formatQuery c q p
       -- Check if change set has already been applied
       existingMigration :: (Maybe Migration) <-
         listToMaybe <$> query sqlFindMigration ( migrationTable
@@ -114,7 +129,7 @@ migrate' tables c schema migrations = do
                                             , mid
                                             , sql
                                             )
-          void $ execute_ $ fromString $ T.unpack sql
+          void $ execute_ $ Query sql
 
   where
 
